07d4867e45b848e38af6896ef2a384902aca7490
Thomas <totojest@gmail.com>
2018-12-10 13:51:29 +0100

    Avant mise en place complète jQuery

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -5,3 +5,6 @@
  */
 let title = document.getElementById('main-title');
 title.innerHTML = 'Hello Javascript';
+
+// Création instance de la classe Login
+const login = new Login(); //déclaration et construction objet login
---
+++ src/user/login.class.js
@@ -0,0 +1,8 @@
+class Login{
+constructor(){
+    // console.log('Je suis le constructeur de Login') // pour tester que le constructeur marche bien
+    // Modifier le titre du document HTML
+    $(document).attr('title', 'Identification'); //$ = objet jQuery, entre (): selecteur, .attr: méthode de jQuery pour attribute
+}
+
+}
