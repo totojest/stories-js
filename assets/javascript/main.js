92651aec512c31e6781e42c5833a204d99a678d4
Thomas <totojest@gmail.com>
2018-12-10 10:29:36 +0100

    Externalisation des scripts

---
+++ assets/javascript/main.js
@@ -0,0 +1,7 @@
+/**
+ * @name main.js
+ * @desc Point d'entrée principal dans l'application Javascript
+ * 
+ */
+let title = document.getElementById('main-title');
+title.innerHTML = 'Hello Javascript';
