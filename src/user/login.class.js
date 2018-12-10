ac514118f7193e4bc445cbc067ecb479f1c66bff
Thomas <totojest@gmail.com>
2018-12-10 14:38:35 +0100

    Gestionnaire d'événements sur le login

--- src/user/login.class.js
+++ src/user/login.class.js
@@ -1,8 +1,39 @@
-class Login{
-constructor(){
-    // console.log('Je suis le constructeur de Login') // pour tester que le constructeur marche bien
-    // Modifier le titre du document HTML
-    $(document).attr('title', 'Identification'); //$ = objet jQuery, entre (): selecteur, .attr: méthode de jQuery pour attribute
-}
+class Login {
+    constructor() {
+        // console.log('Je suis le constructeur de Login') // pour tester que le constructeur marche bien
+        // Modifier le titre du document HTML
+        $(document).attr('title', 'Identification'); //$ = objet jQuery, entre (): selecteur, .attr: méthode de jQuery pour attribute
+
+        //Modifier le titre de la page
+        $('#main-title').html('Identifiez-vous');
+
+        //Définition du listener sur le formulaire
+        this.formListener();
+    }
+
+    /**
+     * formListener: Gestionnaire d'événement sur le formulaire de login (Méthode que l'on définit nous même)
+     * @param void
+     * @return void (Aucun parametre ni de retour)
+     */
+    formListener() {
+        $('#loginForm').on(
+            'keyup', //on écoute tous les changements sur le formulaire
+            function (event) { //callback: fonction appelée si l'événement défini survient
+                //Vérification contenu des champs Login & password
+                const login = $('[name="loginField"]');
+                const password = $('[name="passwordField"]');
+
+                // Les champs sont-ils remplis?
+                if (login.val() !== '' && password.val() !== '') {
+                    $('#btnLogin').removeAttr('disabled');
+                }
+                else {
+                    $('#btnLogin').attr('disabled', 'disabled'); //remettre la valeur disabled sur l'attribut disabled
+                }
+            }
+        );
+    }
+
 
 }
